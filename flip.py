5c6e99b58fca27c89dfa89bcf3383b76f3d5623e
Spencer Smith <ssmith22@usfca.edu>
2016-07-05 11:37:49 -0700

    add new code to flip

--- flip.py
+++ flip.py
@@ -2,9 +2,22 @@ import sys
 from PIL import Image
 
 # define your flip function here
-...
+
+def flip(img):
+	width, height = img.size
+	imgup = img.copy()
+	m = img.load()
+	m2 = imgup.load()
+
+	for x in range(0, width):
+		for y in range(0, height):
+			m2[x, y] = m[width - x - 1, y]
+
+	imgup.show()
+
+
 if len(sys.argv)<=1:
-	print "missing image filename"
+	print ("missing image filename")
 	sys.exit(1)
 filename = sys.argv[1]
 img = Image.open(filename)
@@ -12,7 +25,7 @@ img = img.convert("L")
 img.show()
 
 # call your flip function here
-...
-img.show()
+flip(img)
 
-#to add
+
+
